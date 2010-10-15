cd87fff6f9a963d239f2d65fced86f31d2544604
Clercin Guillaume <gclercin@intellique.com>
2010-10-15 18:36:13 +0200

    Add new checksum => SHA256

--- src/module/checksum/sha256/sha256.c
+++ src/module/checksum/sha256/sha256.c
@@ -24,62 +24,62 @@
 *                                                                       *
 *  -------------------------------------------------------------------  *
 *  Copyright (C) 2010, Clercin guillaume <gclercin@intellique.com>      *
-*  Last modified: Fri, 15 Oct 2010 18:15:55 +0200                       *
+*  Last modified: Fri, 15 Oct 2010 18:24:05 +0200                       *
 \***********************************************************************/
 
 // malloc
 #include <malloc.h>
-// SHA1_Final, SHA1_Init, SHA1_Update
+// SHA256_Final, SHA256_Init, SHA256_Update
 #include <openssl/sha.h>
 
 #include <storiqArchiver/checksum.h>
 
-struct checksum_sha1_private {
-	SHA_CTX sha1;
+struct checksum_sha256_private {
+	SHA256_CTX sha256;
 	short finished;
 };
 
-static char * checksum_sha1_finish(struct checksum * checksum);
-static void checksum_sha1_free(struct checksum * checksum);
-static struct checksum * checksum_sha1_new_checksum(struct checksum_driver * driver, struct checksum * checksum);
-static int checksum_sha1_update(struct checksum * checksum, const char * data, unsigned int length);
+static char * checksum_sha256_finish(struct checksum * checksum);
+static void checksum_sha256_free(struct checksum * checksum);
+static struct checksum * checksum_sha256_new_checksum(struct checksum_driver * driver, struct checksum * checksum);
+static int checksum_sha256_update(struct checksum * checksum, const char * data, unsigned int length);
 
-static struct checksum_driver checksum_sha1_driver = {
-	.name = "sha1",
-	.new_checksum = checksum_sha1_new_checksum,
+static struct checksum_driver checksum_sha256_driver = {
+	.name = "sha256",
+	.new_checksum = checksum_sha256_new_checksum,
 	.data = 0,
 	.cookie = 0,
 };
 
-static struct checksum_ops checksum_sha1_ops = {
-	.finish = checksum_sha1_finish,
-	.free = checksum_sha1_free,
-	.update = checksum_sha1_update,
+static struct checksum_ops checksum_sha256_ops = {
+	.finish = checksum_sha256_finish,
+	.free = checksum_sha256_free,
+	.update = checksum_sha256_update,
 };
 
 
-char * checksum_sha1_finish(struct checksum * checksum) {
-	struct checksum_sha1_private * self = checksum->data;
+char * checksum_sha256_finish(struct checksum * checksum) {
+	struct checksum_sha256_private * self = checksum->data;
 
 	unsigned char digest[SHA_DIGEST_LENGTH];
 
-	short ok = SHA1_Final(digest, &self->sha1);
-	self->finished = 1;
+	short ok = SHA256_Final(digest, &self->sha256);
+	self->finished = 256;
 	if (!ok)
 		return 0;
 
-	char * hexDigest = malloc(SHA_DIGEST_LENGTH * 2 + 1);
+	char * hexDigest = malloc(SHA_DIGEST_LENGTH * 2 + 256);
 	checksum_convert2Hex(digest, SHA_DIGEST_LENGTH, hexDigest);
 
 	return hexDigest;
 }
 
-void checksum_sha1_free(struct checksum * checksum) {
-	struct checksum_sha1_private * self = checksum->data;
+void checksum_sha256_free(struct checksum * checksum) {
+	struct checksum_sha256_private * self = checksum->data;
 
 	if (!self->finished) {
 		unsigned char digest[SHA_DIGEST_LENGTH];
-		SHA1_Final(digest, &self->sha1);
+		SHA256_Final(digest, &self->sha256);
 		self->finished = 1;
 	}
 
@@ -90,26 +90,26 @@ void checksum_sha1_free(struct checksum * checksum) {
 }
 
 __attribute__((constructor))
-static void checksum_sha1_init() {
-	checksum_registerDriver(&checksum_sha1_driver);
+static void checksum_sha256_init() {
+	checksum_registerDriver(&checksum_sha256_driver);
 }
 
-struct checksum * checksum_sha1_new_checksum(struct checksum_driver * driver __attribute__((unused)), struct checksum * checksum) {
+struct checksum * checksum_sha256_new_checksum(struct checksum_driver * driver __attribute__((unused)), struct checksum * checksum) {
 	if (!checksum)
 		checksum = malloc(sizeof(struct checksum));
-	checksum->ops = &checksum_sha1_ops;
+	checksum->ops = &checksum_sha256_ops;
 
-	struct checksum_sha1_private * self = malloc(sizeof(struct checksum_sha1_private));
-	SHA1_Init(&self->sha1);
+	struct checksum_sha256_private * self = malloc(sizeof(struct checksum_sha256_private));
+	SHA256_Init(&self->sha256);
 	self->finished = 0;
 
 	checksum->data = self;
 	return checksum;
 }
 
-int checksum_sha1_update(struct checksum * checksum, const char * data, unsigned int length) {
-	struct checksum_sha1_private * self = checksum->data;
-	if (SHA1_Update(&self->sha1, data, length))
+int checksum_sha256_update(struct checksum * checksum, const char * data, unsigned int length) {
+	struct checksum_sha256_private * self = checksum->data;
+	if (SHA256_Update(&self->sha256, data, length))
 		return length;
 	return -1;
 }
