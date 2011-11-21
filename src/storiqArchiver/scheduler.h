f529915d69c13c7b1cad92885296ae9b2a53273d
Clercin Guillaume <gclercin@intellique.com>
2011-11-21 13:48:18 +0100

    Update licence header

--- src/storiqArchiver/scheduler.h
+++ src/storiqArchiver/scheduler.h
@@ -1,31 +1,29 @@
-/***********************************************************************\
-*       ______           ____     ___           __   _                  *
-*      / __/ /____  ____/  _/__ _/ _ | ________/ /  (_)  _____ ____     *
-*     _\ \/ __/ _ \/ __// // _ `/ __ |/ __/ __/ _ \/ / |/ / -_) __/     *
-*    /___/\__/\___/_/ /___/\_, /_/ |_/_/  \__/_//_/_/|___/\__/_/        *
-*                           /_/                                         *
-*  -------------------------------------------------------------------  *
-*  This file is a part of StorIqArchiver                                *
-*                                                                       *
-*  StorIqArchiver is free software; you can redistribute it and/or      *
-*  modify it under the terms of the GNU General Public License          *
-*  as published by the Free Software Foundation; either version 3       *
-*  of the License, or (at your option) any later version.               *
-*                                                                       *
-*  This program is distributed in the hope that it will be useful,      *
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of       *
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
-*  GNU General Public License for more details.                         *
-*                                                                       *
-*  You should have received a copy of the GNU General Public License    *
-*  along with this program; if not, write to the Free Software          *
-*  Foundation, Inc., 51 Franklin Street, Fifth Floor,                   *
-*  Boston, MA  02110-1301, USA.                                         *
-*                                                                       *
-*  -------------------------------------------------------------------  *
-*  Copyright (C) 2011, Clercin guillaume <gclercin@intellique.com>      *
-*  Last modified: Thu, 30 Jun 2011 21:27:45 +0200                       *
-\***********************************************************************/
+/*************************************************************************\
+*        ______           ____     ___           __   _                   *
+*       / __/ /____  ____/  _/__ _/ _ | ________/ /  (_)  _____ ____      *
+*      _\ \/ __/ _ \/ __// // _ `/ __ |/ __/ __/ _ \/ / |/ / -_) __/      *
+*     /___/\__/\___/_/ /___/\_, /_/ |_/_/  \__/_//_/_/|___/\__/_/         *
+*                            /_/                                          *
+*  ---------------------------------------------------------------------  *
+*  This file is a part of StorIqArchiver                                  *
+*                                                                         *
+*  StorIqArchiver is free software; you can redistribute it and/or        *
+*  modify it under the terms of the GNU General Public License            *
+*  as published by the Free Software Foundation; either version 3         *
+*  of the License, or (at your option) any later version.                 *
+*                                                                         *
+*  This program is distributed in the hope that it will be useful,        *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+*  GNU General Public License for more details.                           *
+*                                                                         *
+*  You should have received a copy of the GNU General Public License      *
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+*                                                                         *
+*  ---------------------------------------------------------------------  *
+*  Copyright (C) 2011, Clercin guillaume <gclercin@intellique.com>        *
+*  Last modified: Mon, 21 Nov 2011 13:47:34 +0100                         *
+\*************************************************************************/
 
 #ifndef __STORIQARCHIVER_SCHEDULER_H__
 #define __STORIQARCHIVER_SCHEDULER_H__
