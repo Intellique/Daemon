6f09d8b3e4c3df751c75898db2730d86512c8296
Clercin Guillaume <gclercin@intellique.com>
2011-12-23 16:21:20 +0100

    Update database

--- script/sql/psql/create_database.sql
+++ script/sql/psql/create_database.sql
@@ -1,7 +1,8 @@
 -- Types
 CREATE TYPE ChangerSlotType AS ENUM (
     'default',
-    'io'
+    'drive',
+    'import / export'
 );
 
 CREATE TYPE ChangerStatus AS ENUM (
@@ -56,6 +57,25 @@ CREATE TYPE JobType AS ENUM (
     'verify'
 );
 
+CREATE TYPE LogLevel AS ENUM (
+    'debug',
+    'info',
+    'warning',
+    'error'
+);
+
+CREATE TYPE LogType AS ENUM (
+    'changer',
+    'daemon',
+    'drive',
+    'job',
+	'plugin log',
+	'plugin db',
+	'plugin log',
+    'ui',
+    'user message'
+);
+
 CREATE TYPE TapeFormatDataType AS ENUM (
     'audio',
     'cleaning',
@@ -71,7 +91,8 @@ CREATE TYPE TapeFormatMode AS ENUM (
 
 CREATE TYPE TapeLocation AS ENUM (
     'offline',
-    'online'
+    'online',
+    'in drive'
 );
 
 CREATE TYPE TapeStatus AS ENUM (
@@ -98,44 +119,46 @@ CREATE TABLE TapeFormat (
     maxOpCount INTEGER NOT NULL CHECK (maxOpCount > 0),
     lifespan INTEGER NOT NULL CHECK (lifespan > 0),
     capacity BIGINT NOT NULL CHECK (capacity > 0),
-    blockSize INTEGER NOT NULL DEFAULT 0 CHECK (blockSize >= 0),
-	densityCode SMALLINT NOT NULL,
+    blockSize INTEGER NOT NULL DEFAULT 0 CHECK (blockSize > 0),
+    densityCode SMALLINT NOT NULL,
     supportPartition BOOLEAN NOT NULL
 );
 
 CREATE TABLE Pool (
     id SERIAL PRIMARY KEY,
+    uuid UUID NOT NULL UNIQUE,
     name VARCHAR(64) NOT NULL,
     retention INTEGER NOT NULL CHECK (retention > 0),
     retentionLimit TIMESTAMP,
     autoRecycle BOOLEAN NOT NULL,
-    tapeFormat_id INTEGER NOT NULL REFERENCES TapeFormat(id) ON DELETE RESTRICT ON UPDATE CASCADE
+    tapeFormat INTEGER NOT NULL REFERENCES TapeFormat(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE Tape (
     id SERIAL PRIMARY KEY,
-    label UUID NULL UNIQUE,
-    name VARCHAR(64) NOT NULL,
+    uuid UUID NULL UNIQUE,
+    label VARCHAR(64) NOT NULL,
+    name VARCHAR(255) NULL,
     status TapeStatus NOT NULL,
     location TapeLocation NOT NULL,
-    firstUsed TIMESTAMP(0),
+    firstUsed TIMESTAMP(0) NOT NULL,
     useBefore TIMESTAMP(0),
     loadCount INTEGER NOT NULL DEFAULT 0 CHECK (loadCount >= 0),
     readCount INTEGER NOT NULL DEFAULT 0 CHECK (readCount >= 0),
     writeCount INTEGER NOT NULL DEFAULT 0 CHECK (writeCount >= 0),
     endPos INTEGER NOT NULL DEFAULT 0 CHECK (endPos >= 0),
     nbFiles INTEGER NOT NULL DEFAULT 0 CHECK (nbFiles >= 0),
-    blockSize INTEGER NOT NULL DEFAULT 0 CHECK (blockSize >= 0),
+    blockSize INTEGER NOT NULL DEFAULT 0 CHECK (blockSize > 0),
     hasPartition BOOLEAN NOT NULL DEFAULT FALSE,
     tapeFormat INTEGER NOT NULL REFERENCES TapeFormat(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    pool INTEGER REFERENCES Pool(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    pool INTEGER REFERENCES Pool(id) ON DELETE SET NULL ON UPDATE CASCADE,
     CHECK (firstUsed < useBefore)
 );
 
 CREATE TABLE DriveFormat (
     id SERIAL PRIMARY KEY,
     name VARCHAR(64) NOT NULL UNIQUE,
-	densityCode SMALLINT NOT NULL,
+    densityCode SMALLINT NOT NULL CHECK (densityCode > 0),
     cleaningInterval INTEGER NOT NULL CHECK (cleaningInterval > 0)
 );
 
@@ -162,6 +185,7 @@ CREATE TABLE Changer (
     model VARCHAR(64) NOT NULL,
     vendor VARCHAR(64) NOT NULL,
     firmwareRev VARCHAR(64) NOT NULL,
+    serialNumber VARCHAR(64) NOT NULL,
     update TIMESTAMP(0) NOT NULL,
     host INTEGER NOT NULL REFERENCES Host(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -169,9 +193,10 @@ CREATE TABLE Changer (
 CREATE TABLE Drive (
     id SERIAL PRIMARY KEY,
     device VARCHAR(64) NOT NULL,
+    scsiDevice VARCHAR(64) NOT NULL,
     status DriveStatus NOT NULL,
     changerNum INTEGER NOT NULL CHECK (changerNum >= 0),
-    operationDuration INTEGER NOT NULL DEFAULT 0 CHECK (operationDuration >= 0),
+    operationDuration FLOAT(3) NOT NULL DEFAULT 0 CHECK (operationDuration >= 0),
     lastClean TIMESTAMP(0) NOT NULL,
     model VARCHAR(64) NOT NULL,
     vendor VARCHAR(64) NOT NULL,
@@ -186,7 +211,26 @@ CREATE TABLE ChangerSlot (
     index INTEGER NOT NULL,
     changer INTEGER NOT NULL REFERENCES Changer(id) ON DELETE CASCADE ON UPDATE CASCADE,
     tape INTEGER REFERENCES Tape(id) ON DELETE SET NULL ON UPDATE CASCADE,
-    CONSTRAINT unique_slot UNIQUE (index,changer)
+    type ChangerSlotType NOT NULL DEFAULT 'default',
+    CONSTRAINT unique_slot UNIQUE (index, changer)
+);
+
+CREATE TABLE SelectedFile (
+    id BIGSERIAL PRIMARY KEY,
+    path VARCHAR(255) NOT NULL,
+    host INTEGER NOT NULL REFERENCES Host(id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE Users (
+    id SERIAL PRIMARY KEY,
+    login VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    salt CHAR(16) NOT NULL,
+    fullname VARCHAR(255),
+    email VARCHAR(255) NOT NULL,
+    isAdmin BOOLEAN NOT NULL DEFAULT FALSE,
+    canArchive BOOLEAN NOT NULL DEFAULT FALSE,
+    canRestore BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE Archive (
@@ -194,6 +238,7 @@ CREATE TABLE Archive (
     name VARCHAR(255) NOT NULL,
     ctime TIMESTAMP(0),
     endtime TIMESTAMP(0),
+    login INTEGER NULL REFERENCES Users(id) ON DELETE SET NULL ON UPDATE CASCADE,
     CONSTRAINT archive_time CHECK (ctime <= endtime)
 );
 
@@ -201,29 +246,31 @@ CREATE TABLE ArchiveFile (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     type FileType NOT NULL,
-    owner SMALLINT NOT NULL DEFAULT 0,
-    groups SMALLINT NOT NULL DEFAULT 0,
-    perm SMALLINT NOT NULL,
+    ownerId SMALLINT NOT NULL DEFAULT 0,
+    owner VARCHAR(255) NOT NULL,
+    groupId SMALLINT NOT NULL DEFAULT 0,
+    groups VARCHAR(255) NOT NULL,
+    perm SMALLINT NOT NULL CHECK (perm >= 0),
     ctime TIMESTAMP(0) NOT NULL,
     mtime TIMESTAMP(0) NOT NULL,
     size BIGINT NOT NULL CHECK (size >= 0)
 );
 
 CREATE TABLE ArchiveVolume (
-    id SERIAL PRIMARY KEY,
-    sequence INTEGER NOT NULL,
-    size BIGINT NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    sequence INTEGER NOT NULL DEFAULT 0 CHECK (sequence >= 0),
+    size BIGINT NOT NULL DEFAULT 0 CHECK (size >= 0),
     ctime TIMESTAMP(0),
     endtime TIMESTAMP(0),
-    tapePosition INTEGER NOT NULL,
-    archive INTEGER NOT NULL REFERENCES Archive(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    tape INTEGER NOT NULL REFERENCES Tape(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CHECK (ctime <= endtime)
+    archive BIGINT NOT NULL REFERENCES Archive(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    tape INTEGER NOT NULL REFERENCES Tape(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    tapePosition INTEGER NOT NULL DEFAULT 0 CHECK (tapePosition >= 0),
+    CONSTRAINT archiveVolume_time CHECK (ctime <= endtime)
 );
 
 CREATE TABLE ArchiveFileToArchiveVolume (
-    archiveVolume INTEGER NOT NULL REFERENCES ArchiveVolume(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    archiveFile INTEGER NOT NULL REFERENCES ArchiveFile(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    archiveVolume BIGINT NOT NULL REFERENCES ArchiveVolume(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    archiveFile BIGINT NOT NULL REFERENCES ArchiveFile(id) ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (archiveVolume, archiveFile)
 );
 
@@ -234,15 +281,9 @@ CREATE TABLE Checksum (
 
 CREATE TABLE ChecksumResult (
     id BIGSERIAL PRIMARY KEY,
-    result VARCHAR(255) NOT NULL,
     checksum INTEGER NOT NULL REFERENCES Checksum(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT ChecksumResult_checksum_invalid UNIQUE (result, checksum)
-);
-
-CREATE TABLE ArchiveVolumeToChecksumResult (
-    archiveVolume INTEGER NOT NULL REFERENCES ArchiveVolume(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    checksumResult INTEGER NOT NULL REFERENCES ChecksumResult(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (archiveVolume, checksumResult)
+    result VARCHAR(255) NOT NULL,
+    CONSTRAINT ChecksumResult_checksum_invalid UNIQUE (checksum, result)
 );
 
 CREATE TABLE ArchiveFileToChecksumResult (
@@ -251,75 +292,58 @@ CREATE TABLE ArchiveFileToChecksumResult (
     PRIMARY KEY (archiveFile, checksumResult)
 );
 
-CREATE TABLE MetaType (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE
-);
-
-CREATE TABLE Meta (
-    id BIGSERIAL PRIMARY KEY,
-    metaType INTEGER NOT NULL REFERENCES MetaType(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    meta_value TEXT NOT NULL
-);
-
-CREATE TABLE ArchiveFileToMeta (
-    archiveFile BIGINT NOT NULL REFERENCES ArchiveFile(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    meta BIGINT NOT NULL REFERENCES Meta(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE Users (
-    id SERIAL PRIMARY KEY,
-    login VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    salt CHAR(16) NOT NULL,
-    fullname VARCHAR(255),
-    email VARCHAR(255) NOT NULL,
-    isAdmin BOOLEAN NOT NULL DEFAULT FALSE
-);
-
-CREATE TABLE SelectedFile (
-    id SERIAL PRIMARY KEY,
-    path VARCHAR(255) NOT NULL,
-    recursive BOOLEAN NOT NULL DEFAULT FALSE,
-    host INTEGER NOT NULL REFERENCES Host(id) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE ArchiveVolumeToChecksumResult (
+    archiveVolume BIGINT NOT NULL REFERENCES ArchiveVolume(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    checksumResult BIGINT NOT NULL REFERENCES ChecksumResult(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (archiveVolume, checksumResult)
 );
 
 CREATE TABLE Job (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     type JobType NOT NULL,
+	done FLOAT NOT NULL DEFAULT 0,
     status JobStatus NOT NULL,
-    start TIMESTAMP(0) NOT NULL,
+    nextStart TIMESTAMP(0) NOT NULL,
     interval INTEGER CHECK (interval > 0),
-    repetition INTEGER NOT NULL DEFAULT 1 CHECK (repetition >= 0),
-    update TIMESTAMP(0) NOT NULL,
-    archive INTEGER REFERENCES Archive(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    repetition INTEGER NOT NULL DEFAULT 1,
+    archive BIGINT REFERENCES Archive(id) ON DELETE CASCADE ON UPDATE CASCADE,
     login INTEGER NOT NULL REFERENCES Users(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    pool INTEGER NULL REFERENCES Pool(id) ON DELETE SET NULL ON UPDATE CASCADE
+    pool INTEGER NULL REFERENCES Pool(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    update TIMESTAMP(0) NOT NULL
 );
 
 CREATE TABLE JobToChecksum (
-    job INTEGER NOT NULL REFERENCES Job(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    job BIGINT NOT NULL REFERENCES Job(id) ON DELETE CASCADE ON UPDATE CASCADE,
     checksum INTEGER NOT NULL REFERENCES Checksum(id) ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (job, checksum)
 );
 
 CREATE TABLE JobRecord (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
+    job BIGINT NOT NULL REFERENCES Job(id) ON DELETE CASCADE ON UPDATE CASCADE,
     status JobStatus NOT NULL CHECK (status != 'disable'),
     numRun INTEGER NOT NULL DEFAULT 1 CHECK (numRun > 0),
     started TIMESTAMP(0) NOT NULL,
     ended TIMESTAMP(0),
     message TEXT,
-    job INTEGER NOT NULL REFERENCES Job(id) ON DELETE CASCADE ON UPDATE CASCADE,
     host INTEGER NOT NULL REFERENCES Host(id) ON DELETE RESTRICT ON UPDATE CASCADE,
     CHECK (started <= ended)
 );
 
 CREATE TABLE JobToSelectedFile (
-    job INTEGER NOT NULL REFERENCES Job(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    selectedFile INTEGER NOT NULL REFERENCES SelectedFile(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    job BIGINT NOT NULL REFERENCES Job(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    selectedFile BIGINT NOT NULL REFERENCES SelectedFile(id) ON DELETE RESTRICT ON UPDATE CASCADE,
     PRIMARY KEY (job, selectedFile)
+);
+
+CREATE TABLE Log (
+    id BIGSERIAL PRIMARY KEY,
+    type LogType NOT NULL,
+    level LogLevel NOT NULL,
+    time TIMESTAMP(0) NOT NULL,
+    message TEXT NOT NULL,
+    login INTEGER NULL REFERENCES Users(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- Comments
--- script/sql/psql/insert_initial_data.sql
+++ script/sql/psql/insert_initial_data.sql
@@ -1,18 +1,20 @@
 INSERT INTO Host VALUES (DEFAULT, 'localhost', NULL),
        (DEFAULT, 'd65', '3U test'),
-       (DEFAULT, 'd190', '5U court');
+       (DEFAULT, 'taiko.intellique.com', '5U court');
 
 INSERT INTO TapeFormat VALUES (DEFAULT, 'LTO-5', 'data', 'linear', 4096, 4096, 40960, 40960, 1024, 1610612736, 8192, 88, TRUE),
        (DEFAULT, 'LTO-4', 'data', 'linear', 4096, 4096, 40960, 40960, 1024, 153691136, 8192, 70, FALSE),
        (DEFAULT, 'LTO-3', 'data', 'linear', 4096, 4096, 40960, 40960, 1024, 153691136, 8192, 68, FALSE),
        (DEFAULT, 'LTO-2', 'data', 'linear', 4096, 4096, 40960, 40960, 1024, 153691136, 8192, 66, FALSE),
-       (DEFAULT, 'X23', 'data', 'linear', 4096, 4096, 40960, 40960, 1024, 153691136, 1024, 130, FALSE);
+       (DEFAULT, 'X23', 'data', 'linear', 4096, 4096, 40960, 40960, 1024, 153691136, 1024, 130, FALSE),
+       (DEFAULT, 'DLT', 'data', 'linear', 4096, 4096, 40960, 40960, 1024, 153691136, 1024, 129, FALSE);
 
 INSERT INTO DriveFormat VALUES (DEFAULT, 'LTO-5', 88, 30),
-	   (DEFAULT, 'LTO-4', 70, 30),
-	   (DEFAULT, 'LTO-3', 68, 30),
-	   (DEFAULT, 'LTO-2', 66, 30),
-       (DEFAULT, 'VXA-3', 130, 30);
+       (DEFAULT, 'LTO-4', 70, 30),
+       (DEFAULT, 'LTO-3', 68, 30),
+       (DEFAULT, 'LTO-2', 66, 30),
+       (DEFAULT, 'VXA-3', 130, 30),
+       (DEFAULT, 'DLT', 129, 30);
 
 INSERT INTO DriveFormatSupport VALUES (1, 1, TRUE, TRUE),
        (1, 2, TRUE, TRUE),
@@ -21,16 +23,18 @@ INSERT INTO DriveFormatSupport VALUES (1, 1, TRUE, TRUE),
        (2, 2, TRUE, TRUE),
        (2, 3, TRUE, TRUE),
        (2, 4, TRUE, FALSE),
-	   (3, 3, TRUE, TRUE),
-	   (3, 4, TRUE, TRUE),
-	   (3, 5, TRUE, FALSE),
-	   (4, 4, TRUE, TRUE),
-	   (5, 5, TRUE, TRUE);
+       (3, 3, TRUE, TRUE),
+       (3, 4, TRUE, TRUE),
+       (3, 5, TRUE, FALSE),
+       (4, 4, TRUE, TRUE),
+       (5, 5, TRUE, TRUE),
+       (5, 6, TRUE, TRUE),
+       (6, 6, TRUE, TRUE);
 
 INSERT INTO CheckSum VALUES (DEFAULT, 'MD5'),
-	   (DEFAULT, 'SHA1'),
-	   (DEFAULT, 'SHA256'),
-	   (DEFAULT, 'SHA512');
+       (DEFAULT, 'SHA1'),
+       (DEFAULT, 'SHA256'),
+       (DEFAULT, 'SHA512');
 
-INSERT INTO Users VALUES (DEFAULT, 'storiq', '09ea7d58393247aae89bdd9bde93a298fc45ed4d', 'storiq', 'storiq@localhost', FALSE);
+INSERT INTO Users VALUES (DEFAULT, 'storiq', '8a6eb1d3b4fecbf8a1d6528a6aecb064e801b1e0', 'cd8c63688e0c2cff', 'storiq', 'storiq@localhost', TRUE, TRUE, TRUE);
 
