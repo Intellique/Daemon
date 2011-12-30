601f287d64b31af495228696183e0e1ffbfbbdbd
Clercin Guillaume <gclercin@intellique.com>
2011-12-30 09:12:37 +0100

    Forgot to add this file

---
+++ src/stone/checksum.h
@@ -0,0 +1,36 @@
+/*************************************************************************\
+*                            __________                                   *
+*                           / __/_  __/__  ___  ___                       *
+*                          _\ \  / / / _ \/ _ \/ -_)                      *
+*                         /___/ /_/  \___/_//_/\__/                       *
+*                                                                         *
+*  ---------------------------------------------------------------------  *
+*  This file is a part of STone                                           *
+*                                                                         *
+*  STone is free software; you can redistribute it and/or                 *
+*  modify it under the terms of the GNU General Public License            *
+*  as published by the Free Software Foundation; either version 3         *
+*  of the License, or (at your option) any later version.                 *
+*                                                                         *
+*  This program is distributed in the hope that it will be useful,        *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+*  GNU General Public License for more details.                           *
+*                                                                         *
+*  You should have received a copy of the GNU General Public License      *
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+*                                                                         *
+*  ---------------------------------------------------------------------  *
+*  Copyright (C) 2011, Clercin guillaume <gclercin@intellique.com>        *
+*  Last modified: Sat, 17 Dec 2011 19:36:55 +0100                         *
+\*************************************************************************/
+
+#ifndef __STONE_CHECKSUM_P_H__
+#define __STONE_CHECKSUM_P_H__
+
+#include <stone/checksum.h>
+
+void st_checksum_sync_plugins(void);
+
+#endif
+
