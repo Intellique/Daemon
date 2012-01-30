f4c7d80d1edf674f505fcd663efbd51d49374e3d
Clercin Guillaume <gclercin@intellique.com>
2012-01-30 17:30:28 +0100

    Update admin functions

--- src/stone/admin.c
+++ src/stone/admin.c
@@ -22,7 +22,7 @@
 *                                                                         *
 *  ---------------------------------------------------------------------  *
 *  Copyright (C) 2011, Clercin guillaume <gclercin@intellique.com>        *
-*  Last modified: Mon, 30 Jan 2012 12:20:57 +0100                         *
+*  Last modified: Mon, 30 Jan 2012 17:23:05 +0100                         *
 \*************************************************************************/
 
 #define _GNU_SOURCE
@@ -34,17 +34,31 @@
 #include <time.h>
 #include <unistd.h>
 
-
 #include <stone/checksum.h>
 #include <stone/io/socket.h>
 #include <stone/library/changer.h>
+#include <stone/library/ressource.h>
+#include <stone/library/tape.h>
 #include <stone/threadpool.h>
 #include <stone/user.h>
 
 #include "admin.h"
 #include "config.h"
 
+struct st_admin_context {
+	char * query;
+
+	struct st_changer * changer;
+
+	json_t * result;
+	json_t * lines;
+};
+
 static short st_admin_do_authen(struct st_io_socket * socket);
+static void st_admin_com_help(struct st_admin_context * context);
+static void st_admin_com_list_changers(struct st_admin_context * context);
+static void st_admin_com_list_slots(struct st_admin_context * context);
+static void st_admin_com_select_changer(struct st_admin_context * context);
 static void st_admin_init(void) __attribute__((constructor));
 static void st_admin_listen(void * arg);
 static void st_admin_work(void * arg);
@@ -201,6 +215,129 @@ short st_admin_do_authen(struct st_io_socket * socket) {
 	return ok;
 }
 
+void st_admin_com_help(struct st_admin_context * context) {
+	json_array_append_new(context->lines, json_string("List of commands:"));
+	json_array_append_new(context->lines, json_string(" h, ?, help:         Show this"));
+	json_array_append_new(context->lines, json_string(" lc, list changers:  List available changers"));
+	json_array_append_new(context->lines, json_string(" ls, list slots:     List available slots of selected changer"));
+	json_array_append_new(context->lines, json_string(" sl, select changer: Select changer by number (see command `cl`)"));
+
+	json_object_set_new(context->result, "status code", json_integer(200));
+	json_object_set_new(context->result, "status", json_string("Ok"));
+}
+
+void st_admin_com_list_changers(struct st_admin_context * context) {
+	struct st_changer * ch = st_changer_get_first_changer();
+	int i;
+	for (i = 0; ch; i++) {
+		char * line = 0;
+		asprintf(&line, "%d. %s %s; rev: %s, serial: %s", i, ch->vendor, ch->model, ch->revision, ch->serial_number);
+
+		json_array_append_new(context->lines, json_string(line));
+
+		free(line);
+
+		ch = st_changer_get_next_changer(ch);
+	}
+	json_object_set_new(context->result, "status code", json_integer(200));
+	json_object_set_new(context->result, "status", json_string("Ok"));
+}
+
+void st_admin_com_list_slots(struct st_admin_context * context) {
+	if (!context->changer) {
+		context->changer = st_changer_get_first_changer();
+		while (context->changer && context->changer->lock->ops->trylock(context->changer->lock))
+			context->changer = st_changer_get_next_changer(context->changer);
+
+		if (!context->changer) {
+			json_array_append_new(context->lines, json_string("No changer available"));
+			json_object_set_new(context->result, "status code", json_integer(410));
+			json_object_set_new(context->result, "status", json_string("Gone"));
+			return;
+		} else {
+			char * line;
+			asprintf(&line, "Auto selecting changer: %s %s, rev: %s, serial: %s", context->changer->vendor, context->changer->model, context->changer->revision, context->changer->serial_number);
+			json_array_append_new(context->lines, json_string(line));
+			free(line);
+		}
+	}
+
+	unsigned int i;
+	for (i = 0; i < context->changer->nb_slots; i++) {
+		struct st_slot * slot = context->changer->slots + i;
+
+		char * line;
+		if (slot->tape) {
+			if (!*slot->tape->label && slot->tape->medium_serial_number)
+				asprintf(&line, "%u. Full: medium serial number %s", i, slot->tape->medium_serial_number);
+			else
+				asprintf(&line, "%u. Full: label: %s", i, slot->tape->label);
+
+		} else {
+			asprintf(&line, "%u. Empty", i);
+		}
+
+		json_array_append_new(context->lines, json_string(line));
+		free(line);
+	}
+
+	json_object_set_new(context->result, "status code", json_integer(200));
+	json_object_set_new(context->result, "status", json_string("Ok"));
+}
+
+void st_admin_com_select_changer(struct st_admin_context * context) {
+	const char * params = context->query;
+	if (params[3] == '\0')
+		params = 0;
+	else if (params[3] == ' ')
+		params += 3;
+	else
+		params += 16;
+
+	int index = 0;
+	if (params)
+		sscanf(params, "%d", &index);
+
+	struct st_changer * ch = st_changer_get_first_changer();
+	int i;
+	for (i = 0; i < index; i++)
+		ch = st_changer_get_next_changer(ch);
+
+	int status;
+	const char * status_string;
+
+	if (ch && ch != context->changer) {
+		if (ch->lock->ops->trylock(ch->lock)) {
+			json_array_append_new(context->lines, json_string("Changer is busy"));
+
+			status = 410;
+			status_string = "Gone";
+		} else {
+			json_array_append_new(context->lines, json_string("Changer is now selected"));
+
+			if (context->changer)
+				context->changer->lock->ops->unlock(context->changer->lock);
+			context->changer = ch;
+
+			status = 200;
+			status_string = "Ok";
+		}
+	} else if (ch) {
+		json_array_append_new(context->lines, json_string("Changer previously selected"));
+
+		status = 200;
+		status_string = "Ok";
+	} else if (!ch) {
+		json_array_append_new(context->lines, json_string("Changer not found"));
+
+		status = 404;
+		status_string = "Not found";
+	}
+
+	json_object_set_new(context->result, "status code", json_integer(status));
+	json_object_set_new(context->result, "status", json_string(status_string));
+}
+
 void st_admin_init() {
 	st_admin_boottime = time(0);
 }
@@ -236,6 +373,11 @@ void st_admin_work(void * arg) {
 	struct st_stream_reader * sr = socket->ops->get_output_stream(socket);
 	struct st_stream_writer * sw = socket->ops->get_input_stream(socket);
 
+	struct st_admin_context context = {
+		.changer = 0,
+		.result  = 0,
+	};
+
 	for (;;) {
 		char buffer[4096];
 		ssize_t nb_read = sr->ops->read(sr, buffer, 4096);
@@ -253,77 +395,79 @@ void st_admin_work(void * arg) {
 			break;
 		}
 
-		const char * squery = json_string_value(query);
+		context.query = strdup(json_string_value(query));
 
 		static const struct st_admin_command {
 			const char * name;
 			enum {
 				COM_HELP = 1,
 				COM_LIST_CHANGERS,
+				COM_LIST_SLOTS,
+				COM_SELECT_CHANGER,
 			} command;
 		} commands[] = {
-			{ "h",             COM_HELP },
-			{ "help",          COM_HELP },
-			{ "cl",            COM_LIST_CHANGERS },
-			{ "list changers", COM_LIST_CHANGERS },
-			{ "?",             COM_HELP },
+			{ "h",              COM_HELP },
+			{ "help",           COM_HELP },
+			{ "lc",             COM_LIST_CHANGERS },
+			{ "list changers",  COM_LIST_CHANGERS },
+			{ "list slots",     COM_LIST_SLOTS },
+			{ "ls",             COM_LIST_SLOTS },
+			{ "select changer", COM_SELECT_CHANGER },
+			{ "sl",             COM_SELECT_CHANGER },
+			{ "?",              COM_HELP },
 
 			{ 0, 0 },
 		};
 
 		const struct st_admin_command * com;
 		for (com = commands; com->name; com++)
-			if (!strcmp(squery, com->name))
+			if (!strncmp(context.query, com->name, strlen(com->name)))
 				break;
 
 		json_decref(query_root);
 
-		json_t * result = json_object();
-		json_t * lines = json_array();
-		json_object_set_new(result, "response", lines);
+		context.result = json_object();
+		context.lines = json_array();
+		json_object_set_new(context.result, "response", context.lines);
 
 		switch (com->command) {
 			case COM_HELP:
-				json_array_append_new(lines, json_string("List of commands:"));
-				json_array_append_new(lines, json_string(" h, ?, help:        Show this"));
-				json_array_append_new(lines, json_string(" cl, list changers: List available changers"));
-
-				json_object_set_new(result, "status", json_integer(200));
+				st_admin_com_help(&context);
 				break;
 
-			case COM_LIST_CHANGERS: {
-					struct st_changer * changer = st_changer_get_first_changer();
-					int i;
-					for (i = 0; changer; i++) {
-						char * line = 0;
-						asprintf(&line, "%d. %s %s; rev: %s, serial: %s", i, changer->vendor, changer->model, changer->revision, changer->serial_number);
+			case COM_LIST_CHANGERS:
+				st_admin_com_list_changers(&context);
+				break;
 
-						json_array_append_new(lines, json_string(line));
+			case COM_LIST_SLOTS:
+				st_admin_com_list_slots(&context);
+				break;
 
-						free(line);
-
-						changer = st_changer_get_next_changer(changer);
-					}
-					json_object_set_new(result, "status", json_integer(200));
-				}
+			case COM_SELECT_CHANGER:
+				st_admin_com_select_changer(&context);
 				break;
 
 			default:
-				json_array_append_new(lines, json_string("Command not found"));
-				json_object_set_new(result, "status", json_integer(404));
+				json_array_append_new(context.lines, json_string("Command not found"));
+				json_object_set_new(context.result, "status code", json_integer(404));
+				json_object_set_new(context.result, "status", json_string("Not found"));
 				break;
 		}
 
-		char * message = json_dumps(result, 0);
+		char * message = json_dumps(context.result, 0);
 
 		ssize_t nb_write = sw->ops->write(sw, message, strlen(message));
 
 		free(message);
-		json_decref(result);
+		json_decref(context.result);
+		free(context.query);
 
 		if (nb_write < 0)
 			break;
 	}
+
+	if (context.changer)
+		context.changer->lock->ops->unlock(context.changer->lock);
 
 	sr->ops->free(sr);
 	sw->ops->free(sw);
