a397acaa91df3eca7f35b1fe42616152caad5f29
Clercin Guillaume <gclercin@intellique.com>
2012-06-01 19:15:47 +0200

    Add script to restore hardlink

---
+++ script/restore-link.pl
@@ -0,0 +1,18 @@
+#! /usr/bin/perl
+
+use strict;
+use warnings;
+
+open( my $fd, '<', 'script/link.txt' ) or die 'no link file';
+
+while (<$fd>) {
+	chomp;
+
+    if (/^(.+) -> (.+)$/) {
+		unlink $2 if -f $2;
+		link $1, $2 unless -e $2;
+    }
+}
+
+close $fd;
+
