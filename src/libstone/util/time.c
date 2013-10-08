92306ed4b502c9a0ace316aef6c26ca3951e7547
Clercin Guillaume <gclercin@intellique.com>
2013-10-08 10:29:10 +0200

    Add convenience function for time

---
+++ src/libstone/util/time.c
@@ -0,0 +1,44 @@
+/****************************************************************************\
+*                             __________                                     *
+*                            / __/_  __/__  ___  ___                         *
+*                           _\ \  / / / _ \/ _ \/ -_)                        *
+*                          /___/ /_/  \___/_//_/\__/                         *
+*                                                                            *
+*  ------------------------------------------------------------------------  *
+*  This file is a part of STone                                              *
+*                                                                            *
+*  STone is free software; you can redistribute it and/or modify             *
+*  it under the terms of the GNU Affero General Public License               *
+*  as published by the Free Software Foundation; either version 3            *
+*  of the License, or (at your option) any later version.                    *
+*                                                                            *
+*  This program is distributed in the hope that it will be useful,           *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU Affero General Public License for more details.                       *
+*                                                                            *
+*  You should have received a copy of the GNU Affero General Public License  *
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
+*                                                                            *
+*  ------------------------------------------------------------------------  *
+*  Copyright (C) 2013, Clercin guillaume <gclercin@intellique.com>           *
+*  Last modified: Wed, 05 Jun 2013 14:58:08 +0200                            *
+\****************************************************************************/
+
+// localtime_r, time
+#include <time.h>
+
+#include <libstone/util/time.h>
+
+size_t st_util_time_convert(time_t * clock, const char * format, char * buffer, size_t buffer_length) {
+	struct tm lnow;
+	if (clock == NULL) {
+		time_t lclock = time(NULL);
+		localtime_r(&lclock, &lnow);
+	} else {
+		localtime_r(clock, &lnow);
+	}
+
+	return strftime(buffer, buffer_length, format, &lnow);
+}
+
