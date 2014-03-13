6a63259b4ca93a7cd25b1392eeca30596090c6c3
Clercin Guillaume <gclercin@intellique.com>
2014-03-13 17:51:28 +0100

    Improve create proxy
    Use all cpus available to create proxy.
    Require perl module: Sys::CPU (debian package: libsys-cpu-perl)
    
    (cherry picked from commit e0586d09de7315ef9a5da82acfd23999442fe183)

--- script/pre-post-run/create-proxy.pl
+++ script/pre-post-run/create-proxy.pl
@@ -3,9 +3,12 @@
 use strict;
 use warnings;
 
-use Digest::MD5 qw(md5_hex);
+use Digest::MD5 q/md5_hex/;
 use JSON::PP;
+use POSIX q/nice/;
+use Sys::CPU q/cpu_count/;
 
+my $nb_cpu     = cpu_count();
 my $output_dir = '/var/www/stone/cache/movies/proxy';
 
 my $param = shift;
@@ -37,6 +40,7 @@ unless ($encoder) {
 my $archive = decode_json $data_in;
 
 my %file_done;
+my $nb_processes = 0;
 foreach my $vol ( @{ $archive->{volumes} } ) {
     foreach my $file ( @{ $vol->{files} } ) {
         next if defined $file_done{ $file->{path} };
@@ -44,11 +48,29 @@ foreach my $vol ( @{ $archive->{volumes} } ) {
 
         next unless $file->{'mime type'} =~ m{^video/};
 
-        my $filename = md5_hex( $file->{path} ) . '.mp4';
-        my $status = system $encoder, '-v', 'quiet', '-i', $file->{path}, '-acodec',
-            'libvorbis', '-ac', '2', '-ab', '96k', '-ar', '44100', '-b',
-            '500k', '-s', '320x240', '-t', '0:0:30', "$output_dir/$filename";
+        my $pid = fork();
+        if ( $pid == 0 ) {
+            nice(10);
+
+            my $filename = md5_hex( $file->{path} ) . '.mp4';
+            exec $encoder, '-v', 'quiet', '-i', $file->{path}, '-acodec',
+                'libvorbis', '-ac', '2', '-ab', '96k', '-ar', '44100', '-b',
+                '500k', '-s', '320x240', '-t', '0:0:30',
+                "$output_dir/$filename";
+        }
+
+        $nb_processes++;
+
+        if ($nb_processes >= $nb_cpu) {
+            wait();
+            $nb_processes--;
+        }
     }
+}
+
+while ($nb_processes > 0) {
+    wait();
+    $nb_processes--;
 }
 
 my $sent = {
