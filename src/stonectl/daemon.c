f8061ce4c52a4da7c7a78a8836ed6cad723d5ee4
Clercin Guillaume <gclercin@intellique.com>
2014-03-20 22:57:13 +0100

    Add new command stonectl
    design to manage daemon
    
    can lanch daemon

---
+++ src/stonectl/daemon.c
@@ -0,0 +1,41 @@
+/****************************************************************************\
+*                             __________                                     *
+*                            / __/_  __/__  ___  ___                         *
+*                           _\ \  / / / _ \/ _ \/ -_)                        *
+*                          /___/ /_/  \___/_//_/\__/                         *
+*                                                                            *
+*  ------------------------------------------------------------------------  *
+*  This file is a part of STone                                              *
+*                                                                            *
+*  STone is free software; you can redistribute it and/or modify             *
+*  it under the terms of the GNU Affero General Public License               *
+*  as published by the Free Software Foundation; either version 3            *
+*  of the License, or (at your option) any later version.                    *
+*                                                                            *
+*  This program is distributed in the hope that it will be useful,           *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU Affero General Public License for more details.                       *
+*                                                                            *
+*  You should have received a copy of the GNU Affero General Public License  *
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
+*                                                                            *
+*  ------------------------------------------------------------------------  *
+*  Copyright (C) 2014, Clercin guillaume <gclercin@intellique.com>           *
+\****************************************************************************/
+
+// NULL
+#include <stddef.h>
+
+#include <libstone/process.h>
+
+#include "common.h"
+
+int stctl_start_daemon(int argc __attribute__((unused)), char ** argv __attribute__((unused))) {
+	struct st_process daemon;
+	st_process_new(&daemon, "./bin/stoned", NULL, 0);
+	st_process_start(&daemon, 1);
+
+	return daemon.pid < 2;
+}
+
---
+++ src/stonectl/main.c
@@ -0,0 +1,58 @@
+/****************************************************************************\
+*                             __________                                     *
+*                            / __/_  __/__  ___  ___                         *
+*                           _\ \  / / / _ \/ _ \/ -_)                        *
+*                          /___/ /_/  \___/_//_/\__/                         *
+*                                                                            *
+*  ------------------------------------------------------------------------  *
+*  This file is a part of STone                                              *
+*                                                                            *
+*  STone is free software; you can redistribute it and/or modify             *
+*  it under the terms of the GNU Affero General Public License               *
+*  as published by the Free Software Foundation; either version 3            *
+*  of the License, or (at your option) any later version.                    *
+*                                                                            *
+*  This program is distributed in the hope that it will be useful,           *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU Affero General Public License for more details.                       *
+*                                                                            *
+*  You should have received a copy of the GNU Affero General Public License  *
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
+*                                                                            *
+*  ------------------------------------------------------------------------  *
+*  Copyright (C) 2014, Clercin guillaume <gclercin@intellique.com>           *
+\****************************************************************************/
+
+// printf
+#include <stdio.h>
+
+#include <libstone/value.h>
+
+#include "common.h"
+
+static void init(void) __attribute__((constructor));
+
+static struct st_value * commands = NULL;
+
+
+static void init() {
+	commands = st_value_new_hashtable2();
+	st_value_hashtable_put2(commands, "start", st_value_new_custom(stctl_start_daemon, NULL), true);
+}
+
+int main(int argc, char ** argv) {
+	if (argc < 1)
+		return 1;
+
+	if (!st_value_hashtable_has_key2(commands, argv[1])) {
+		printf("Error: command not found (%s)\n", argv[1]);
+		return 2;
+	}
+
+	struct st_value * com = st_value_hashtable_get2(commands, argv[1], false);
+	command_f command = com->value.custom.data;
+
+	return command(argc - 1, argv + 1);
+}
+
