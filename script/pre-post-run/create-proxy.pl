9977d0564d97158205dedbe8a9ea2231a8a033cd
Clercin Guillaume <gclercin@intellique.com>
2014-04-24 23:55:44 +0200

    Improve create proxy script
    Create also .ogv files
    
    (cherry picked from commit 8ee517f3a1ca6e9864c8f7dfbffc8f0f5e6c8bc5)

--- script/pre-post-run/create-proxy.pl
+++ script/pre-post-run/create-proxy.pl
@@ -41,6 +41,28 @@ my $archive = decode_json $data_in;
 
 my %file_done;
 my $nb_processes = 0;
+
+sub process {
+    my ( $input, $format ) = @_;
+
+    my $pid = fork();
+    if ( $pid == 0 ) {
+        nice(10);
+
+        my $filename = md5_hex($input) . '.mp4';
+        exec $encoder, '-v', 'quiet', '-i', $input, '-acodec', 'libvorbis',
+            '-ac', '2', '-ab', '96k', '-ar', '44100', '-b', '500k', '-s',
+            '320x240', '-t', '0:0:30', "$output_dir/$filename";
+    }
+
+    $nb_processes++;
+
+    if ( $nb_processes >= $nb_cpu ) {
+        wait();
+        $nb_processes--;
+    }
+}
+
 foreach my $vol ( @{ $archive->{volumes} } ) {
     foreach my $file ( @{ $vol->{files} } ) {
         next if defined $file_done{ $file->{path} };
@@ -48,27 +70,12 @@ foreach my $vol ( @{ $archive->{volumes} } ) {
 
         next unless $file->{'mime type'} =~ m{^video/};
 
-        my $pid = fork();
-        if ( $pid == 0 ) {
-            nice(10);
-
-            my $filename = md5_hex( $file->{path} ) . '.mp4';
-            exec $encoder, '-v', 'quiet', '-i', $file->{path}, '-acodec',
-                'libvorbis', '-ac', '2', '-ab', '96k', '-ar', '44100', '-b',
-                '500k', '-s', '320x240', '-t', '0:0:30',
-                "$output_dir/$filename";
-        }
-
-        $nb_processes++;
-
-        if ($nb_processes >= $nb_cpu) {
-            wait();
-            $nb_processes--;
-        }
+        process( $file->{path}, '.mp4' );
+        process( $file->{path}, '.ogv' );
     }
 }
 
-while ($nb_processes > 0) {
+while ( $nb_processes > 0 ) {
     wait();
     $nb_processes--;
 }
