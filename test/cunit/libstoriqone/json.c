bffcf2672aa558372318ef449f5b4cd3a4af294d
Clercin Guillaume <gclercin@intellique.com>
2014-11-11 15:33:00 +0100

    Miss to add json test files

---
+++ test/cunit/libstoriqone/json.c
@@ -0,0 +1,77 @@
+/****************************************************************************\
+*                    ______           _      ____                            *
+*                   / __/ /____  ____(_)__ _/ __ \___  ___                   *
+*                  _\ \/ __/ _ \/ __/ / _ `/ /_/ / _ \/ -_)                  *
+*                 /___/\__/\___/_/ /_/\_, /\____/_//_/\__/                   *
+*                                      /_/                                   *
+*  ------------------------------------------------------------------------  *
+*  This file is a part of Storiq One                                         *
+*                                                                            *
+*  Storiq One is free software; you can redistribute it and/or modify        *
+*  it under the terms of the GNU Affero General Public License               *
+*  as published by the Free Software Foundation; either version 3            *
+*  of the License, or (at your option) any later version.                    *
+*                                                                            *
+*  This program is distributed in the hope that it will be useful,           *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU Affero General Public License for more details.                       *
+*                                                                            *
+*  You should have received a copy of the GNU Affero General Public License  *
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
+*                                                                            *
+*  ------------------------------------------------------------------------  *
+*  Copyright (C) 2014, Clercin guillaume <gclercin@intellique.com>           *
+\****************************************************************************/
+
+#include "json.h"
+
+// CU_add_suite, CU_cleanup_registry, CU_cleanup_registry
+#include <CUnit/CUnit.h>
+// printf
+#include <stdio.h>
+// free
+#include <stdlib.h>
+// _exit
+#include <unistd.h>
+
+#include <libstoriqone/json.h>
+#include <libstoriqone/value.h>
+
+static void test_libstoriqone_json_parse_string_0(void);
+
+static struct {
+	void (*function)(void);
+	char * name;
+} test_functions[] = {
+    { test_libstoriqone_json_parse_string_0, "libstoriqone: json parse string: #0" },
+
+	{ 0, 0 },
+};
+
+
+void test_libstoriqone_json_add_suite() {
+	CU_pSuite suite = CU_add_suite("libstoriqone: json", 0, 0);
+	if (!suite) {
+		CU_cleanup_registry();
+		printf("Error while adding suite libstoriqone because %s\n", CU_get_error_msg());
+		_exit(3);
+	}
+
+	int i;
+	for (i = 0; test_functions[i].name; i++) {
+		if (!CU_add_test(suite, test_functions[i].name, test_functions[i].function)) {
+			CU_cleanup_registry();
+			printf("Error while adding test function '%s' libstoriqone because %s\n", test_functions[i].name, CU_get_error_msg());
+			_exit(3);
+		}
+	}
+}
+
+static void test_libstoriqone_json_parse_string_0() {
+	static const char * json = "{\"density code\":1,\"foo\":\"bar\"}";
+	struct so_value * value = so_json_parse_string(json);
+    CU_ASSERT_PTR_NOT_NULL_FATAL(value);
+	so_value_free(value);
+}
+
