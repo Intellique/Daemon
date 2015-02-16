1697886d55dc293cec69ca336b8a7aef55617708
Clercin Guillaume <gclercin@intellique.com>
2015-02-16 11:38:40 +0100

    Importation de bande storiq one

--- src/libstone/library/archive.c
+++ src/libstone/library/archive.c
@@ -25,10 +25,15 @@
 *  Last modified: Fri, 07 Feb 2014 10:23:34 +0100                            *
 \****************************************************************************/
 
+#define _XOPEN_SOURCE 500
+// sscanf
+#include <stdio.h>
 // free, malloc
 #include <stdlib.h>
 // strcmp, strdup
 #include <string.h>
+// bzero
+#include <strings.h>
 // struct stat
 #include <sys/stat.h>
 // time
@@ -36,9 +41,14 @@
 // uuid_generate, uuid_unparse_lower
 #include <uuid/uuid.h>
 
+#include <libstone/job.h>
 #include <libstone/library/archive.h>
+#include <libstone/library/media.h>
+#include <libstone/user.h>
 #include <libstone/util/file.h>
 #include <libstone/util/hashtable.h>
+#include <libstone/util/string.h>
+#include <libstone/util/util.h>
 
 static struct st_archive_file_type2 {
 	char * name;
@@ -105,6 +115,8 @@ struct st_archive * st_archive_new(const char * name, struct st_user * user) {
 	uuid_generate(id);
 
 	struct st_archive * archive = malloc(sizeof(struct st_archive));
+	bzero(archive, sizeof(struct st_archive));
+
 	uuid_unparse_lower(id, archive->uuid);
 	archive->name = strdup(name);
 
@@ -118,6 +130,150 @@ struct st_archive * st_archive_new(const char * name, struct st_user * user) {
 	archive->db_data = NULL;
 
 	return archive;
+}
+
+struct st_archive * st_archive_parse(json_t * jarchive, unsigned int position) {
+	struct st_job_selected_path * path = malloc(sizeof(struct st_job_selected_path));
+	path->path = strdup("/");
+	path->db_data = NULL;
+
+	struct st_archive * archive = malloc(sizeof(struct st_archive));
+	bzero(archive, sizeof(struct st_archive));
+
+	json_t * uuid = json_object_get(jarchive, "uuid");
+	strncpy(archive->uuid, json_string_value(uuid), 36);
+	archive->uuid[36] = '\0';
+
+	json_t * name = json_object_get(jarchive, "name");
+	archive->name = strdup(json_string_value(name));
+
+	json_t * volumes = json_object_get(jarchive, "volumes");
+	archive->nb_volumes = json_array_size(volumes);
+	archive->volumes = calloc(archive->nb_volumes, sizeof(struct st_archive_volume));
+
+	unsigned int i;
+	bool ok = true;
+	for (i = 0; i < archive->nb_volumes; i++) {
+		struct st_archive_volume * vol = archive->volumes + i;
+		json_t * jvol = json_array_get(volumes, i);
+
+		json_unpack(jvol, "{sisi}",
+			"sequence", &vol->sequence,
+			"size", &vol->size,
+			"nb files", &vol->nb_files
+		);
+
+		json_t * jtext = json_object_get(jvol, "created time");
+		struct tm time;
+		strptime(json_string_value(jtext), "%F %T", &time);
+		vol->start_time = mktime(&time);
+
+		jtext = json_object_get(jvol, "finish time");
+		strptime(json_string_value(jtext), "%F %T", &time);
+		vol->end_time = mktime(&time);
+
+		vol->check_ok = false;
+		vol->check_time = 0;
+		vol->archive = archive;
+		vol->job = NULL;
+
+		json_t * jmedia = json_object_get(jvol, "media");
+		name = json_object_get(jmedia, "medium serial number");
+
+		vol->media = st_media_get_by_medium_serial_number(json_string_value(name));
+		ok = vol->media != NULL;
+
+		if (archive->nb_volumes == 1)
+			vol->media_position = position;
+		else
+			ok = false;
+
+		json_t * jdigests = json_object_get(jvol, "checksums");
+		vol->digests = st_hashtable_new2(st_util_string_compute_hash, st_util_basic_free);
+
+		const char * key;
+		json_t * value;
+		json_object_foreach(jdigests, key, value) {
+			st_hashtable_put(vol->digests, strdup((char *) key), st_hashtable_val_string((char *) json_string_value(value)));
+		}
+
+		json_t * jfiles = json_object_get(jvol, "files");
+		vol->nb_files = json_array_size(jfiles);
+
+		unsigned int j;
+		vol->files = calloc(vol->nb_files, sizeof(struct st_archive_files));
+		for (j = 0; j < vol->nb_files; j++) {
+			struct st_archive_files * files = vol->files + j;
+			json_t * jfile = json_array_get(jfiles, j);
+
+			json_unpack(jfile, "{si}", "block position", &files->position);
+
+			struct st_archive_file * file = files->file = malloc(sizeof(struct st_archive_file));
+			bzero(file, sizeof(struct st_archive_file));
+			jfile = json_object_get(jfile, "file");
+
+			jtext = json_object_get(jfile, "name");
+			file->name = strdup(json_string_value(jtext));
+
+			jtext = json_object_get(jfile, "permission");
+			const char * text = json_string_value(jtext);
+			sscanf(text, "%d", &file->perm);
+
+			jtext = json_object_get(jfile, "type");
+			file->type = st_archive_file_string_to_type(json_string_value(jtext));
+
+			jtext = json_object_get(jfile, "owner");
+			strncpy(file->owner, json_string_value(jtext), 32);
+
+			jtext = json_object_get(jfile, "group");
+			strncpy(file->group, json_string_value(jtext), 32);
+
+			json_unpack(jfile, "{sisisi}",
+				"owner id", &file->ownerid,
+				"group id", &file->groupid,
+				"size", &file->size
+			);
+
+			jtext = json_object_get(jfile, "created time");
+			strptime(json_string_value(jtext), "%F %T", &time);
+			file->create_time = mktime(&time);
+
+			jtext = json_object_get(jfile, "modified time");
+			strptime(json_string_value(jtext), "%F %T", &time);
+			file->modify_time = mktime(&time);
+
+			file->archived_time = 0;
+			file->check_ok = false;
+			file->check_time = 0;
+
+			jtext = json_object_get(jfile, "mime type");
+			file->mime_type = strdup(json_string_value(jtext));
+
+			jdigests = json_object_get(jfile, "checksums");
+			file->digests = st_hashtable_new2(st_util_string_compute_hash, st_util_basic_free);
+
+			const char * key;
+			json_t * value;
+			json_object_foreach(jdigests, key, value) {
+				st_hashtable_put(file->digests, strdup((char *) key), st_hashtable_val_string((char *) json_string_value(value)));
+			}
+
+			file->archive = archive;
+			file->selected_path = path;
+		}
+	}
+
+	name = json_object_get(jarchive, "user");
+	archive->user = st_user_get(json_string_value(name));
+
+	json_t * metas = json_object_get(jarchive, "metadatas");
+	archive->metadatas = json_dumps(metas, JSON_COMPACT);
+
+	if (ok)
+		return archive;
+
+	st_archive_free(archive);
+	return NULL;
 }
 
 void st_archive_file_free(struct st_archive_file * file) {
