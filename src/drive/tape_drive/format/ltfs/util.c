0fe47eff0144b1ff2061a2dd4df5086d93026d83
Clercin Guillaume <gclercin@intellique.com>
2015-05-07 18:43:25 +0200

    Ouli de nouveaux fichiers

---
+++ src/drive/tape_drive/format/ltfs/util.c
@@ -0,0 +1,50 @@
+/****************************************************************************\
+*                    ______           _      ____                            *
+*                   / __/ /____  ____(_)__ _/ __ \___  ___                   *
+*                  _\ \/ __/ _ \/ __/ / _ `/ /_/ / _ \/ -_)                  *
+*                 /___/\__/\___/_/ /_/\_, /\____/_//_/\__/                   *
+*                                      /_/                                   *
+*  ------------------------------------------------------------------------  *
+*  This file is a part of Storiq One                                         *
+*                                                                            *
+*  Storiq One is free software; you can redistribute it and/or modify        *
+*  it under the terms of the GNU Affero General Public License               *
+*  as published by the Free Software Foundation; either version 3            *
+*  of the License, or (at your option) any later version.                    *
+*                                                                            *
+*  This program is distributed in the hope that it will be useful,           *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU Affero General Public License for more details.                       *
+*                                                                            *
+*  You should have received a copy of the GNU Affero General Public License  *
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
+*                                                                            *
+*  ------------------------------------------------------------------------  *
+*  Copyright (C) 2013-2015, Guillaume Clercin <gclercin@intellique.com>      *
+\****************************************************************************/
+
+#define _XOPEN_SOURCE
+// bzero
+#include <strings.h>
+// mktime, strptime, tzset
+#include <time.h>
+
+#include "../ltfs.h"
+
+static void sodr_tape_drive_format_ltfs_init(void) __attribute__((constructor));
+
+
+static void sodr_tape_drive_format_ltfs_init() {
+	tzset();
+}
+
+time_t sodr_tape_drive_format_ltfs_parse_time(const char * date) {
+	struct tm tm;
+	bzero(&tm, sizeof(tm));
+
+	strptime(date, "%FT%T", &tm);
+
+	return mktime(&tm) - timezone;
+}
+
