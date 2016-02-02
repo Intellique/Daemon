c80a7ab3342d8a94d4f5ef3b385a833830d4e67f
Clercin Guillaume <gclercin@intellique.com>
2016-02-02 22:15:35 +0100

    Correctif #84 : Exécution des tâches d'utilisateur désactivé
    On démarre quand même la tâche mais on met un message explicite comme
    quoi la tâche ne s'exécute pas parce que l'utilisateur qui a créé la
    tâche a été désactivé depuis.
    
    (cherry picked from commit 40f3d24c0e3f32b4c37e3010445c118535b7b370)

--- src/database/postgresql/connection.c
+++ src/database/postgresql/connection.c
@@ -107,6 +107,7 @@ static int so_database_postgresql_add_job_record(struct so_database_connection *
 static int so_database_postgresql_add_job_record2(struct so_database_connection * connect, const char * job_id, unsigned int num_run, enum so_job_status status, enum so_log_level level, enum so_job_record_notif notif, const char * message);
 static int so_database_postgresql_add_report(struct so_database_connection * connect, struct so_job * job, struct so_archive * archive, struct so_media * media, const char * data);
 static char * so_database_postgresql_get_restore_path(struct so_database_connection * connect, struct so_job * job);
+static bool so_database_postgresql_get_is_user_disabled(struct so_database_connection * connect, struct so_job * job);
 static int so_database_postgresql_start_job(struct so_database_connection * connect, struct so_job * job);
 static int so_database_postgresql_stop_job(struct so_database_connection * connect, struct so_job * job);
 static int so_database_postgresql_sync_job(struct so_database_connection * connect, struct so_job * job);
@@ -189,6 +190,7 @@ static struct so_database_connection_ops so_database_postgresql_connection_ops =
 	.add_job_record2  = so_database_postgresql_add_job_record2,
 	.add_report       = so_database_postgresql_add_report,
 	.get_restore_path = so_database_postgresql_get_restore_path,
+	.is_user_disabled = so_database_postgresql_get_is_user_disabled,
 	.start_job        = so_database_postgresql_start_job,
 	.stop_job         = so_database_postgresql_stop_job,
 	.sync_job         = so_database_postgresql_sync_job,
@@ -2473,6 +2475,38 @@ static char * so_database_postgresql_get_restore_path(struct so_database_connect
 	PQclear(result);
 
 	return path;
+}
+
+static bool so_database_postgresql_get_is_user_disabled(struct so_database_connection * connect, struct so_job * job) {
+	if (connect == NULL || job == NULL)
+		return false;
+
+	struct so_database_postgresql_connection_private * self = connect->data;
+
+	struct so_value * key = so_value_new_custom(connect->config, NULL);
+	struct so_value * db = so_value_hashtable_get(job->db_data, key, false, false);
+	so_value_free(key);
+
+	char * job_id = NULL;
+	so_value_unpack(db, "{ss}", "id", &job_id);
+
+	const char * query = "check_user_is_enabled";
+	so_database_postgresql_prepare(self, query, "SELECT disabled FROM users wWHERE id = (SELECT login FROM job WHERE id = $1 LIMIT 1)");
+
+	const char * param[] = { job_id };
+	PGresult * result = PQexecPrepared(self->connect, query, 1, param, NULL, NULL, 0);
+	ExecStatusType status = PQresultStatus(result);
+
+	bool disabled = true;
+	if (status == PGRES_FATAL_ERROR)
+		so_database_postgresql_get_error(result, query);
+	else if (status == PGRES_TUPLES_OK && PQntuples(result) == 1)
+		so_database_postgresql_get_bool(result, 0, 0, &disabled);
+
+	PQclear(result);
+	free(job_id);
+
+	return disabled;
 }
 
 static int so_database_postgresql_start_job(struct so_database_connection * connect, struct so_job * job) {
