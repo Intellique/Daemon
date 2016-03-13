0bf982c270d133518e47f8e638fdb533fab3c178
Clercin Guillaume <gclercin@intellique.com>
2016-03-13 10:57:57 +0100

    Add backup documentation

--- include/libstoriqone/backup.h
+++ include/libstoriqone/backup.h
@@ -34,28 +34,89 @@
 
 struct so_value;
 
+/**
+ * \struct so_backup
+ * \brief Represents a backup of database
+ */
 struct so_backup {
+	/**
+	 * \brief backup time
+	 */
 	time_t timestamp;
+	/**
+	 * \brief number of medias at this backup
+	 */
 	long nb_medias;
+	/**
+	 * \brief number of archives at this backup
+	 */
 	long nb_archives;
 
+	/**
+	 * \struct so_backup_volume
+	 * \brief volume's information related to this backup
+	 *
+	 * \var so_backup::volumes
+	 * \brief volumes in current backup
+	 */
 	struct so_backup_volume {
+		/**
+		 * \brief Media of current volume
+		 */
 		struct so_media * media;
+		/**
+		 * \brief File position in current media
+		 *
+		 * \note Usefull when media is a tape
+		 */
 		unsigned int position;
 
+		/**
+		 * \brief size of this volume
+		 */
 		size_t size;
 
+		/**
+		 * \brief result of last check
+		 */
 		time_t checktime;
+		/**
+		 * \brief Last volume check time.
+		 *
+		 * \note Time is store as Unix timestamp.
+		 *
+		 * \note If \a check_time equals to \b 0 then it means
+		 * than this volume has never been checked
+		 */
 		bool checksum_ok;
 
+		/**
+		 * \brief An hashtable which contains digests of this volume.
+		 *
+		 * In this hashtable, keys are algorithm and values are digests.
+		 */
 		struct so_value * digests;
 
+		/**
+		 * \brief private data used by database
+		 */
 		struct so_value * db_data;
 	} * volumes;
+	/**
+	 * \brief Number of volumes of archive
+	 */
 	unsigned int nb_volumes;
 
+	/**
+	 * \brief Job which create this backup
+	 *
+	 * \warning MAY BE NULL
+	 */
 	struct so_job * job;
 
+	/**
+	 * \brief private data used by database
+	 */
 	struct so_value * db_data;
 };
 
