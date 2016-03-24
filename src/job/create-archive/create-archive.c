bb0575cbd7faecbc73d8dceed4d05cb48b852e4a
Clercin Guillaume <gclercin@intellique.com>
2016-03-24 13:50:00 +0100

    Typo fix
    (cherry picked from commit 37965174cf79c16fa1c587039d18b0189aba6c76)

--- src/job/create-archive/create-archive.c
+++ src/job/create-archive/create-archive.c
@@ -211,7 +211,7 @@ static int soj_create_archive_run(struct so_job * job, struct so_database_connec
 
 	soj_job_add_record(job, db_connect, so_log_level_info, so_job_record_notif_normal,
 		dgettext("storiqone-job-create-archive", "Generating report"));
-		
+
 	soj_create_archive_worker_generate_report(selected_path, db_connect);
 
 	bool check_archive = false;
