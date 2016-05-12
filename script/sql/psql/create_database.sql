c018c997e28c42919acad04b7006935336e23a3d
emmanuel <eflorac@intellique.com>
2016-05-12 16:37:56 +0200

    Correction bug interface #474 taiga

--- script/sql/psql/create_database.sql
+++ script/sql/psql/create_database.sql
@@ -775,7 +775,7 @@ CREATE OR REPLACE FUNCTION log_metadata() RETURNS TRIGGER AS $body$
             RAISE EXCEPTION 'type of metadata should not be modified' USING ERRCODE = '09000';
         ELSIF TG_OP = 'DELETE' OR OLD != NEW THEN
             INSERT INTO MetadataLog(id, type, key, value, login, updated)
-                VALUES (OLD.id, OLD.type, OLD.key, OLD.value, OLD.login, TG_OP == 'UPDATE');
+                VALUES (OLD.id, OLD.type, OLD.key, OLD.value, OLD.login, CASE WHEN TG_OP = 'UPDATE' THEN true ELSE false END );
         END IF;
         RETURN NEW;
     END;
