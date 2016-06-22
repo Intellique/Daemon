f63d9ba4f5f194e8ec60488c1bb7c99e21f91364
Clercin Guillaume <gclercin@intellique.com>
2016-06-22 10:30:05 +0200

    New bug fix release

--- config.h
+++ config.h
@@ -28,13 +28,13 @@
 #define __STORIQONE_CONFIG_H__
 
 #define DAEMON_CONFIG_FILE "/etc/storiq/storiqone.conf"
-#define DAEMON_CRASH_DIR "/var/crash/storiqone"
+#define DAEMON_CRASH_DIR "tmp/crash"
 #define DAEMON_SOCKET_DIR "/var/run/storiqone"
 #define DAEMON_PID_FILE "/var/run/storiqone.pid"
 #define DAEMON_BIN_DIR "/usr/lib/storiqone/1.2/bin"
 #define DAEMON_JOB_DIR "/usr/lib/storiqone/1.2/job"
 #define LOCALE_DIR "/usr/share/locale/"
-#define MODULE_PATH "/usr/lib/storiqone/1.2/lib"
+#define MODULE_PATH "lib"
 #define SCRIPT_PATH "/usr/lib/storiqone/1.2/scripts"
 #define TMP_DIR "/tmp"
 
