abd2e6b50a4a4746cfb0ec4844890a0aabab9bc0
emmanuel <eflorac@intellique.com>
2020-07-27 17:38:56 +0200

    ajout script EditShare restore

---
+++ script/pre-post-run/sync-editshare-after-restore.pl
@@ -0,0 +1,97 @@
+#! /usr/bin/perl
+
+use strict;
+use warnings;
+
+use JSON::PP;
+use File::Basename;
+use Data::Dumper;
+use LWP::UserAgent;
+
+if ( @ARGV and $ARGV[0] eq 'config' ) {
+    my $sent = {
+        'name'        => 'ES Flow scan post restore',
+        'description' => 'EditShare Flow Scan after restoring files',
+        'type'        => 'post job'
+    };
+    print encode_json($sent);
+    exit;
+}
+
+my $data_in       = do { local $/; <STDIN> };
+my $data          = decode_json $data_in;
+my $restored_path = $data->{'restore path'};
+
+my $schema     = 'https';
+my $hostname   = '192.168.0.154:8006';
+my $path       = 'api/v2/scan/asset';
+my $login      = 'emmanuel';
+my $password   = 'emmanuel';
+my $mediaspace = 'StoriqOne';
+
+# debug stuff
+# open( my $fd, '>', "/tmp/Rest$$.json" );
+# print {$fd} $data_in;
+# close $fd;
+
+#log stuff
+# open( $fd, '>', "/tmp/Rest$$.log" );
+#
+
+my $message = '';
+my $restpaths;
+
+if ( defined($restored_path) ) {
+    for my $vol ( @{ $data->{archive}->{volumes} } ) {
+        for my $file ( @{ $vol->{files} } ) {
+            next unless defined $file->{file}{'restored to'};
+			next if $file->{file}{type} eq 'directory';
+			
+            my $parent = dirname( $file->{file}{'restored to'} );
+            $restpaths->{$parent}++;
+        }
+
+        foreach my $path ( keys %$restpaths ) {
+			my $parent = dirname $path ;
+            delete $restpaths->{$path}
+              if ( exists $restpaths->{$parent} );
+        }
+    }
+
+} else {
+    $message = "Restore path NOT FOUND $restored_path\n";
+}
+
+my $content_request = encode_json(
+    {   'createproxy' => JSON::PP::true,
+        'files'       =>  [ keys %$restpaths ],
+        'fullscan'    => JSON::PP::false,
+        'mediaspace'  => $mediaspace,
+        'metadata'    => {},
+        'update_pmr'  => JSON::PP::false
+    }
+);
+
+print Dumper $content_request;
+
+my $user_agent = LWP::UserAgent->new;
+$user_agent->ssl_opts( verify_hostname => 0 );
+$user_agent->ssl_opts( SSL_verify_mode => 0 );
+$user_agent->credentials( $hostname, 'Restricted Area', $login, $password );
+
+my $request = HTTP::Request->new( POST => "$schema://$hostname/$path" );
+$request->content_type('application/json');
+$request->content($content_request);
+
+my $result = $user_agent->request($request);
+unless ( $result->is_success ) {
+	$message = "Request to api ($schema://$hostname/$path) failed";
+}
+
+
+my $sent = {
+    'finished' => JSON::PP::true,
+    'data'     => {},
+    'message'  => $message
+};
+print encode_json($sent);
