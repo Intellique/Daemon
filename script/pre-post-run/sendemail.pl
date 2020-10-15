3ba0632096dac5e030b790636916b8263da8ec93
emmanuel <eflorac@intellique.com>
2020-10-15 19:42:34 +0200

    fonctionne bien

--- script/pre-post-run/sendemail.pl
+++ script/pre-post-run/sendemail.pl
@@ -24,22 +24,45 @@ if ( @ARGV and $ARGV[0] eq 'config' ) {
 my $data_in = do { local $/; <STDIN> };
 my $data    = decode_json $data_in;
 
-my $jobname = $data->{job}{name};
-my $jobtype = $data->{job}{type};
-my $jobuser = $data->{job}{user};
+my $jobname  = $data->{job}{name};
+my $jobtype  = $data->{job}{type};
+my $jobuser  = $data->{job}{user};
 my $jobemail = $data->{job}{email};
 
 my $jobstatus = $data->{job}{status};
 
+# for RESTORE and CHECK job, we must compute a report checksum status
+my $report = '';
+
+if ( exists $data->{archive}{volumes} ) {
+    foreach my $vol ( @{ $data->{archive}{volumes} } ) {
+        if ( $vol->{"checksum time"} > 0 and $vol->{"checksum ok"} == 0 ) {
+            $report .=
+              'Volume ' . $vol->{sequence} . " FAILED. Checksum mismatch.\n";
+        }
+
+        foreach my $ptrfile ( @{ $vol->{files} } ) {
+            my $file = $ptrfile->{file};
+            if (    $file->{"checksum time"} > 0
+                and $file->{"checksum ok"} eq 'false' )
+            {
+                $report .=
+                  "File '" . $file->{path} . "' FAILED. Checksum mismatch.\n";
+            }
+        }
+    }
+}
+
 my $message = Email::Simple->create(
-	header => [
-		To => "$jobuser <$jobemail>",
-		From => $senderemail,
-		Subject => "Job '$jobname' done with status '$jobstatus'",
-	],
-	body => "The Storiq One job '$jobname' of type '$jobtype' is done.
-	Status : $jobstatus\n"
-	
+    header => [
+        To      => "$jobuser <$jobemail>",
+        From    => $senderemail,
+        Subject => "Job '$jobname' done with status '$jobstatus'",
+    ],
+    body => "The Storiq One job '$jobname' of type '$jobtype' is done.
+	Status : $jobstatus
+	Report: \n$report"
+
 );
 
 sendmail($message);
@@ -47,6 +70,6 @@ sendmail($message);
 my $sent = {
     'finished' => JSON::PP::true,
     'data'     => {},
-    'message'  => ''
+    'message'  => $report
 };
 print encode_json($sent);
