c5a7691e9196622e34eb388b78f60270e6859054
Guillaume Clercin <gclercin@intellique.com>
2021-01-27 11:44:47 +0100

    Update copyright

--- include/libstoriqone/crash.h
+++ include/libstoriqone/crash.h
@@ -21,7 +21,7 @@
 *  along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
 *                                                                            *
 *  ------------------------------------------------------------------------  *
-*  Copyright (C) 2013-2019, Guillaume Clercin <gclercin@intellique.com>      *
+*  Copyright (C) 2013-2021, Guillaume Clercin <gclercin@intellique.com>      *
 \****************************************************************************/
 
 #ifndef __LIBSTORIQONE_CRASH_H__
